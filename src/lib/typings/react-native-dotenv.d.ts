d6e4a40d5c36a746f71c898d107a7c3b0c5a6b72
Jesse Onolememen <jesseono91@gmail.com>
2020-05-06 17:33:57 +0100

    feat: add react-native-dotenv

---
+++ src/lib/typings/react-native-dotenv.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-native-dotenv' {
+  // Declare all your .env variable types here so typescript compiler knows to find them
+  // See https://github.com/zetachang/react-native-dotenv/issues/76
+}
